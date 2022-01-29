ce10598bf23eef3d1a6e1bcc62241dbc8aa8078f
jrwj97 <jrwj97@aol.com>
2022-01-28 23:15:33 -0500

    added local storage persistance through reloading

--- assets/script.js
+++ assets/script.js
@@ -13,29 +13,37 @@ function timeColor() {
     var currentHour = parseInt($(this).attr("id"));
 
     if (currentHour > hour) {
-      $this.addClass("future");
+      $(this).addClass("future");
     } else if (currentHour === hour) {
-      $this.addClass("present")
+      $(this).addClass("present")
     } else {
-      $this.addClass("past")
+      $(this).addClass("past")
     }
   })
 }
 
 // function to save current text when save button is hit
 saveBtn.on("click", function() {
-  var time = $(this).siblings(".hour").text
-  var appointment = $(this).siblings(".plan").text
+  var time = $(this).siblings(".hour").text();
+  var appointment = $(this).siblings(".plan").val();
 
 
   localStorage.setItem(time, appointment)
 })
 
-function loadSchedule() {
-  $(".hour").each(function() {
-    var hour = $(this).text();
-    var plan = $(this).text();
+// calls the saved data from the localstorage to populate the time blocks
+function savedTimes() {
 
-    
-  })
+  $(".hour").each(function() {
+      var hour = $(this).text();
+      var plan = localStorage.getItem(hour);
+
+      if(plan !== null) {
+          $(this).siblings(".plan").val(plan);
+      }
+  });
 }
+
+// calling the two functions to color the planner and load your previous saves
+timeColor();
+savedTimes();
