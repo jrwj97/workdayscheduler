c9198db23b015a25161e74214713462febb0872b
jrwj97 <jrwj97@aol.com>
2022-01-28 22:48:26 -0500

    added js to color each hour of the day

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,21 @@ var saveBtn = $(".saveBtn");
 // set current day at top of page
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
+// function to change each hour block to past, present, or future
+function timeColor() {
+  var hour = moment().hours();
+
+  // targeting each hour block and running it through a if else statement
+  $(".hour").each(function() {
+    var currentHour = parseInt($(this).attr("id"));
+
+    if (currentHour > hour) {
+      $this.addClass("future");
+    } else if (currentHour === hour) {
+      $this.addClass("present")
+    } else {
+      $this.addClass("past")
+    }
+  })
+}
+
